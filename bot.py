6441533f16cf54de5a04198639a6beab3be3b39f
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-11-14 16:46:22 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
+import random
 
+class Bot(Player):
+    def make_move(self, board):
+        while True:
+            x, y = random.randint(0, 2), random.randint(0, 2)
+            if board[x][y] is None:
+                return x, y
