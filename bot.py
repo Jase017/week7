1791571577168089f4e9e2a16109293a539d458a
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-11-14 16:54:39 -0800

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class Bot():
-    def make_move(self, board):
-        while True:
-            x, y = random.randint(0, 2), random.randint(0, 2)
-            if board[x][y] is None:
-                return x, y
