67963384c1d5dbcaf830aba9cceb79bfc30191ce
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-11-14 16:57:39 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,6 +1,46 @@
 # logic.py
 import random
 
+class Game:
+    def __init__(self):
+        symbols = ['X', 'O']
+        random.shuffle(symbols)
+
+        num_of_players = None
+        while num_of_players not in ['1', '2']:
+            num_of_players = input("Enter the number of players (1/2): ")
+
+        if num_of_players == '1':
+            self.player1 = Player(symbols[0])
+            self.player2 = Bot(symbols[1])
+        elif num_of_players == '2':
+            self.player1 = Player(symbols[0])
+            self.player2 = Player(symbols[1])
+
+        self.board = Board()
+        self.current_player = self.player1
+
+    def play(self):
+        while not self.board.get_winner() and not self.board.is_draw():
+            self.board.print_board()
+            print('Next turn: ', self.current_player.symbol)
+            x, y = self.current_player.make_move(self.board.board)
+            if x == 'q' and y == 'q':
+                print("Game exited by the player.")
+                break
+            self.board.board[x][y] = self.current_player.symbol
+            self.current_player = self.player1 if self.current_player == self.player2 else self.player2
+
+        winner = self.board.get_winner()
+
+        self.board.print_board()
+
+        if winner:
+            print(winner, ' Won')
+        elif self.board.is_draw():
+            print("It's a draw.")
+
+
 
 class Board:
     def __init__(self):
